f8e943ce734e6b76295617dac5f69ef704a69bbb
VSShlenskiy <slenskijvitalij199@gmail.com>
2026-03-02 13:31:45 +0500

    6laba

---
+++ sem2/6 lab/6 laba/6 laba.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <cctype>
+
+using namespace std;
+
+void convert(string& str) {
+    for (int i = 0; i < str.length(); i++) {
+        if (!(isspace(str[i]))) {
+            if (isalpha(str[i]) || str[i] == '_') {
+                while (str[i] != ' ' && i < str.length()) {
+                    i++;
+                }
+            }
+            else {
+                int l = i, len = 0;
+                while (!(isspace(str[i])) && i < str.length()) {
+                    i++;
+                    len++;
+                }
+                str.erase(l, len);
+                i = l;
+            }
+        }
+    }
+
+}
+
+int main() {
+    string s;
+    while (1) {
+        getline(cin, s);
+        convert(s);
+        cout << s << "\n\n";
+    }
+    return 0;
+}
